1d0a56e6e4fee44737280b450d1bdba96f546443
karriqi <bruno.karriqi@sisal.al>
2024-04-15 18:36:16 +0200

    add scroll function and fixed height for item and container

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,33 @@
 import './App.css'
 import ticketData from '../TicketData/tickets.json';
+import {useState} from "react";
 
 function App() {
+  const [scroll, setScroll] = useState(0);
+  const itemHeight = 70;
+  const containerHeight = 700;
+
+  const handleScroll = (event) => {
+    setScroll(event.target.scrollTop);
+  }
 
   return (
     <>
-      <div>
-        <h1>Tickets List</h1>
-        <div className={'listContainer'}>
-          {ticketData.map((item) => (
-            <div className={'listItem'}>
-              <p>{item.description}</p>
-            </div>
-          ))}
+      <h1>Ticket List</h1>
+      <div
+        onScroll={handleScroll}
+        style={{ height: `${containerHeight}px`, overflowY: "scroll" }}
+      >
+        <div style={{ height: `${ticketData.length * itemHeight}px` }}>
+          <div
+            className={'listContainer'}
+          >
+            {ticketData.map((item) => (
+              <div key={item._id} className={'listItem'} style={{height: `${itemHeight}px`}}>
+                <p>{item.description}</p>
+              </div>
+            ))}
+          </div>
         </div>
       </div>
     </>
